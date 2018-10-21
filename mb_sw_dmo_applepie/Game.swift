2dfbefb7c5f49cabc89434577b76d1556c9f68b1
Eric Ampire <ericampire.top@gmail.com>
2018-10-22 00:16:53 +0200

    Ajout d'un camputed proprety a la structure Game ayant pour role de representer le mot a deviner avec les caractères déjà deviner

--- mb_sw_dmo_applepie/Game.swift
+++ mb_sw_dmo_applepie/Game.swift
@@ -12,6 +12,19 @@ struct Game {
     var word: String
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord.append(letter)
+            } else {
+                guessedWord.append("_")
+            }
+        }
+        
+        return guessedWord
+    }
     
     mutating func playerGuessed(letter: Character) {
         guessedLetters.append(letter)
