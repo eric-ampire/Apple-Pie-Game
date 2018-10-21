d12759eca48a4bbbc1c83116603f9c46a80b2db2
Eric Ampire <ericampire.top@gmail.com>
2018-10-21 23:12:01 +0200

    Mise en place du model représentant la partie courante du jeux

---
+++ mb_sw_dmo_applepie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  mb_sw_dmo_applepie
+//
+//  Created by Eric Ampire on 21/10/2018.
+//  Copyright © 2018 Eric Ampire. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
